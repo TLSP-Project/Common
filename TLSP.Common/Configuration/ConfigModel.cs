6b7f698f9a9cf8e65dcb448f5ee0dc08622f8d6c
AvalonD <106223124+AvalonD@users.noreply.github.com>
2022-06-11 08:05:33 +0800

    添加config模块

---
+++ TLSP.Common/Configuration/ConfigModel.cs
@@ -0,0 +1,42 @@
+﻿
+using System.ComponentModel;
+using System.IO;
+
+namespace TLSP.Common.Configuration
+{
+    public class ConfigModel<T> where T : INotifyPropertyChanged
+    {
+
+        public static T? Data { get; private set; }
+
+        public string FilePath { get;  }
+
+        public ConfigModel(string path)
+        {
+            FilePath = path;
+            Flush();
+        }
+
+        public void Flush()
+        {
+            try
+            {
+                Data =  JsonHelper.ReadFormFile<T>(FilePath);
+                Data.PropertyChanged += (sender, args) => Save();
+            }
+            catch 
+            {
+                Data = default(T);
+            }
+        }
+
+        public void Save()
+        {
+            if(Data != null)
+                JsonHelper.WriteToFile(FilePath, Data);
+            else
+                File.WriteAllText(FilePath, "");
+
+        }
+    }
+}
---
+++ TLSP.Common/Utilities/JsonHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+using System.IO;
+namespace TLSP.Common.Utilities
+{
+    public static class JsonHelper
+    {
+        public static T ReadFormFile<T>(string path) => JsonSerializer.Deserialize<T>(File.ReadAllText(path));
+        public static void WriteToFile<T>(string path, T entity) => File.WriteAllText(path, JsonSerializer.Serialize(entity));
+    }
+}
